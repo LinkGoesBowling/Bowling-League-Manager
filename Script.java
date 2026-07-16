927d7c62acd8f962891c702c64f6f6bee5cfe8a7
LinkKelly <54011287+LinkGoesBowling@users.noreply.github.com>
2026-07-15 18:55:31 -0600

    Added handicap calculation
    Need to fix the .0 at the end of numbers when logged in console

--- Script.java
+++ Script.java
@@ -1,14 +1,18 @@
 import java.util.Arrays;
 public class Script {
+    public float baseScore = 220f; //handicap base score and percent
+    public float percent = 90f;
     public class Bowler {
         String name;
-        int pins;
+        double pins;
         int highGame;
         int highSeries;
-        int gameCount;
+        double gameCount;
         String gender;
-        int avg;
-        public Bowler(String name, int pins, int highGame, int highSeries, int gameCount, String gender) {
+        double avg;
+        double hdcp;
+        double roundedAvg;
+        public Bowler(String name, double pins, int highGame, int highSeries, double gameCount, String gender) {
             this.name = name;
             this.pins = pins;
             this.highGame = highGame;
@@ -25,26 +29,31 @@ Bowler[] bowlers = {
         new Bowler ("Naomi", 7020, 289, 725, 36, "F"),
         new Bowler ("Emily", 8136, 300, 780, 36, "F")
 };
-    public void getAvg(){
-        for (int i = 0; i < bowlers.length; i++) {
+    public void calculateAvgAndHdcp(){
+        for (int i = 0; i < bowlers.length; i++){
+            bowlers[i].roundedAvg = Math.floor(bowlers[i].pins / bowlers[i].gameCount);
             bowlers[i].avg = bowlers[i].pins / bowlers[i].gameCount;
+            bowlers[i].hdcp = Math.floor((baseScore - bowlers[i].roundedAvg) * (percent / 100d));
+            if (bowlers[i].avg >= baseScore){
+                bowlers[i].hdcp = 0;
+            }
         }
     }
     public void listBowlers(String gender){
-        getAvg();
+        calculateAvgAndHdcp();
         for (int j = 0; j < bowlers.length; j++) {
             if ((gender == "all") || (gender == "M" && bowlers[j].gender == "M") || (gender == "F" && bowlers[j].gender == "F")) {
-                Arrays.sort(bowlers, (a, b) -> Integer.compare(b.avg, a.avg));
+                Arrays.sort(bowlers, (a, b) -> Double.compare(b.avg, a.avg));
             }
         }
-        for (int i = 0; i < bowlers.length; i++){
+        for (int i = 0; i < bowlers.length; i++){ //there is 2 loops because the 2nd one was to only list top 3, but it wasn't working
             if ((gender == "all") || (gender == "M" && bowlers[i].gender == "M") || (gender == "F" && bowlers[i].gender == "F")) {
-                System.out.println(bowlers[i].name + " " + bowlers[i].avg);
+                System.out.println(bowlers[i].name + " " + bowlers[i].roundedAvg + " " + bowlers[i].hdcp);
             }
         }
     }
     public static void main(String[] args) {
         Script script = new Script();
-        script.listBowlers("all"); //change this for gender filtering
+        script.listBowlers("all");
     }
 }
