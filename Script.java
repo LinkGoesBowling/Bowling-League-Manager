92c796877436728f0b4fd59f6b6f083e3b7258e8
LinkKelly <54011287+LinkGoesBowling@users.noreply.github.com>
2026-07-18 12:51:58 -0600

    Plane ride updates
    Still pretty broken but made some progress

--- Script.java
+++ Script.java
@@ -1,10 +1,10 @@
-import java.util.Arrays;
 import java.text.DecimalFormat;
 import java.util.Scanner;
 import java.util.ArrayList;
 public class Script {
     public float baseScore = 220f; //handicap base score and percent
     public float percent = 90f;
+    public int gamesPerWeek = 0;
     public class Bowler {
         String name;
         double pins;
@@ -16,6 +16,7 @@ public class Script {
         double hdcp;
         double roundedAvg;
         int teamId;
+
         public Bowler(String name, double pins, int highGame, int highSeries, double gameCount, String gender, int teamId) {
             this.name = name;
             this.pins = pins;
@@ -26,91 +27,102 @@ public class Script {
             this.teamId = teamId;
         }
     }
+
     ArrayList<Bowler> bowlers = new ArrayList<>();
-    public void calculateAvgAndHdcp(){
-        for (int i = 0; i < bowlers.size(); i++){
+    public void userChoice(){
+        Scanner reader = new Scanner(System.in);
+        System.out.println("What do you want to do?");
+        System.out.println("Type B to add new bowlers");
+        System.out.println("Type G to add this week's games");
+        System.out.println("Type A to list top 3 bowlers");
+        System.out.println("Type M to list top 3 male bowlers");
+        System.out.println("Type F to list top 3 female bowlers");
+        String choice = reader.next();
+        if (choice.toUpperCase().equals("B")){
+            addNewBowlers();
+        }
+        if (choice.toUpperCase().equals("G")){
+            askGamesPerWeek();
+        }
+        if (choice.toUpperCase().equals("A")){
+            listBowlers("all");
+        }
+        if (choice.toUpperCase().equals("M")){
+            listBowlers("M");
+        }
+        if (choice.toUpperCase().equals("F")){
+            listBowlers("F");
+        }
+    }
+    public void askGamesPerWeek(){
+        if (gamesPerWeek == 0) {
+            Scanner reader = new Scanner(System.in);
+            System.out.println("How many games do you want per week?");
+            gamesPerWeek = reader.nextInt();
+        }
+        addGames();
+    }
+    public void calculateAvgAndHdcp() {
+        for (int i = 0; i < bowlers.size(); i++) {
             bowlers.get(i).roundedAvg = Math.floor(bowlers.get(i).pins / bowlers.get(i).gameCount); //used to show to user. bowling averages always round down
             bowlers.get(i).avg = bowlers.get(i).pins / bowlers.get(i).gameCount; //used to rank bowlers accurately
             bowlers.get(i).hdcp = Math.floor((baseScore - bowlers.get(i).roundedAvg) * (percent / 100d));
-            if (bowlers.get(i).avg >= baseScore){
+            if (bowlers.get(i).avg >= baseScore) {
                 bowlers.get(i).hdcp = 0;
             }
         }
     }
-    public void listBowlers(String gender) {
+
+    public void addNewBowlers() {
         Scanner reader = new Scanner(System.in);
         System.out.println("Enter bowler's name: ");
         String nameInput = reader.next();
         System.out.println("Enter bowler's gender(M/F): ");
-        String genderInput = reader.next();
-        if ((genderInput.equals("M")) || (genderInput.equals("F"))){
+        String genderInput = reader.next().toUpperCase();
+        if ((genderInput.toUpperCase().equals("M")) || (genderInput.toUpperCase().equals("F"))) {
             System.out.println("Enter bowler's team: ");
             int teamInput = reader.nextInt();
-            System.out.println("Are these details correct?");
+            System.out.println("Are these details correct? Y/N:");
             System.out.println("Name: " + nameInput);
             System.out.println("Gender: " + genderInput);
             System.out.println("Team: " + teamInput);
             String confirm = reader.next();
-            if (confirm.equals("Y")){
-                Bowler newBowler = new Bowler(nameInput, 0d, 0, 0, 0d, genderInput, teamInput);
-                System.out.println("Enter bowler's name: ");
-                nameInput = reader.next();
-                System.out.println("Enter bowler's gender(M/F): ");
-                genderInput = reader.next();
-                if ((genderInput.equals("M")) || (genderInput.equals("F"))){
-                    System.out.println("Enter bowler's team: ");
-                    teamInput = reader.nextInt();
-                    System.out.println("Are these details correct?");
-                    System.out.println("Name: " + nameInput);
-                    System.out.println("Gender: " + genderInput);
-                    System.out.println("Team: " + teamInput);
-                    confirm = reader.next();
-                    if (confirm.equals("Y")){
-                        newBowler = new Bowler(nameInput, 0d, 0, 0, 0d, genderInput, teamInput);
-                    }
-                    else{
-                        nameInput = "";
-                        genderInput = "";
-                        teamInput = 0;
-                    }
+            if (confirm.toUpperCase().equals("Y")) {
+                bowlers.add(new Bowler(nameInput, 0d, 0, 0, 0d, genderInput, teamInput));
+                System.out.println("Bowler successfully added");
+                System.out.println("Add another bowler? Y/N");
+                String confirm2 = reader.next();
+                if (confirm2.toUpperCase().equals("Y")) {
+                    addNewBowlers();
                 }
-                else{
-                    System.out.println("Not a valid gender!");
+                if (confirm2.toUpperCase().equals("N")) {
+                    userChoice();
                 }
             }
             else{
-                nameInput = "";
-                genderInput = "";
-                teamInput = 0;
-                System.out.println("Enter bowler's name: ");
-                nameInput = reader.next();
-                System.out.println("Enter bowler's gender(M/F): ");
-                genderInput = reader.next();
-                if ((genderInput.equals("M")) || (genderInput.equals("F"))){
-                    System.out.println("Enter bowler's team: ");
-                   teamInput = reader.nextInt();
-                    System.out.println("Are these details correct?");
-                    System.out.println("Name: " + nameInput);
-                    System.out.println("Gender: " + genderInput);
-                    System.out.println("Team: " + teamInput);
-                    confirm = reader.next();
-                    if (confirm.equals("Y")){
-                        Bowler newBowler = new Bowler(nameInput, 0d, 0, 0, 0d, genderInput, teamInput);
-                    }
-                    else{
-                        nameInput = "";
-                        genderInput = "";
-                        teamInput = 0;
-                    }
-                }
-                else{
-                    System.out.println("Not a valid gender!");
-                }
+                addNewBowlers();
             }
         }
         else{
             System.out.println("Not a valid gender!");
+            addNewBowlers();
         }
+    }
+    public void addGames(){
+        for (int i = 0; i < bowlers.size(); i++){
+            Scanner reader = new Scanner(System.in);
+            System.out.println(bowlers.get(i).name);
+            for (int j = 0; j < gamesPerWeek; j++){
+                System.out.println("Enter game " + (j + 1) + ":");
+                double addPins = reader.nextInt();
+                bowlers.get(j).pins = addPins;
+                bowlers.get(j).gameCount += 1;
+                continue;
+            }
+            userChoice();
+        }
+    }
+    public void listBowlers(String gender) {
         calculateAvgAndHdcp();
         for (int j = 0; j < bowlers.size(); j++) {
             if (gender == "all" || (gender == "M" && bowlers.get(j).gender == "M") || (gender == "F" && bowlers.get(j).gender == "F")) {
@@ -126,8 +138,9 @@ public class Script {
             }
         }
     }
+
     public static void main(String[] args) {
         Script script = new Script();
-        script.listBowlers("all");
+        script.userChoice();
     }
 }
